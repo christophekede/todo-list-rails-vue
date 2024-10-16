c36082478e64ba2c7fc1c422db67b7e2ac366cb1
ahmadou-ai <ahmadou.dia@skello.io>
2024-10-16 16:45:12 +0200

    crud completed

--- server/app/controllers/todo_controller.rb
+++ server/app/controllers/todo_controller.rb
@@ -1,22 +1,52 @@
 class TodoController < ApplicationController
   def index
+    puts "hello"
+    @todos = Todo.all
+    render json: @todos
   end
 
   def new
+    @todo = Todo.new
+    render json: @todo
   end
 
   def show
+    @todo = Todo.find(params[:id])
+    render json: @todo
   end
 
   def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors
+    end
   end
 
   def edit
+    @todo = Todo.find(params[:id])
+    render json: @todo
   end
 
   def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: @todo.errors
+    end
   end
 
   def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    render json: @todo
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :description)
   end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   get 'todo/index'
   get 'todo/new'
-  get 'todo/show'
+  get 'todo/show/:id', to: 'todo#show'
   get 'todo/create'
   get 'todo/edit'
   get 'todo/update'
   get 'todo/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root "home#index"
   get "home/index"
 end
